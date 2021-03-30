a24b30cb43077d859be8291cdd1c7d4581bdeda9
Mateusz <matyd356@gmail.com>
2021-03-30 23:07:12 +0200

    add express validator

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser')
 
 const app = express();
 
+//miiddlewares
 app.use(express.urlencoded({
   extended: false
 }));
 app.use(express.json());
-
 
 //routes
 app.use('/posts', postsRouter);
@@ -19,6 +19,7 @@ app.use('/comments', commentsRouter);
 
 //conectiong to DB
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
 //connection error handling
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -1,17 +1,39 @@
-exports.list_post = (req, res) => {
-  //get all comments from db
-  res.json({ comments: 'comments' })
-}
+const Comment = require('../models/comment');
 
+const { body, validate, validationResult } = require('express-validator');
+//get all comments from db
+exports.list_post = (req, res) => {
+  Comment.find(function (err, comments) {
+    if (err) { return next(err) }
+    console.log(comments)
+    res.json({ comments })
+  })
+}
+//get particular commment from db
 exports.get_comment = (req, res) => {
   res.json({ comment: `comment ${req.params.id}` })
 }
+//add coment to db
+exports.add_comment = [
+  body('title', 'Add a title').trim().isLength({ min: 1 }).escape(),
+  body('message', 'Add comment body').trim().isLength({ min: 1 }).escape(),
+  body('author', 'Add comment author name').trim().isLength({ min: 1 }).escape(),
+  body('authorEmail', 'Add comment author email').trim().isLength({ min: 1 }).escape(),
+  (req, res, next) => {
 
-exports.add_comment = (req, res) => {
-  res.json({
-    comment: {
-      title: req.body.title,
-      message: req.body.message
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({ errors: errors.array() })
+    } else {
+      const commentDraft = new Comment({
+        title: req.body.title,
+        body: req.body.message,
+        author: req.body.author,
+        authorEmail: req.body.authorEmail,
+      })
+      commentDraft.save(function (err) {
+        if (err) { return next(err) }
+        res.json({ ok: 'done' })
+      })
     }
-  })
-}
+  }]
